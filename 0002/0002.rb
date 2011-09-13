2e2fe035682010edbdce2140a65eb8bd7ba1ebcb
Anderson Mesquita <andersonvom@gmail.com>
2011-09-12 23:00:20 -0400

    0002: Optimized ruby solution and improved test cases

--- 0002/0002.rb
+++ 0002/0002.rb
@@ -1,50 +1,95 @@
 #!/usr/bin/ruby
 
-def is_prime(x)
-  # Previously optimized
-  # return false if x == 1
-  # return true  if x == 2
+# Finds all prime numbers up to Math.sqrt(x)
+# using eratosthenes sieve method recursively
+def eratosthenes(x)
+  return [] if x <= 1
+  return [2] if x == 2
 
-  # Optimize first well known primes
-  return true if [3,5,7,11,13,17,19].include? x
+  all_primes = eratosthenes(Math.sqrt(x).to_i)
+  bools = Array.new x+1, true
 
-  # It is known that if no dividers are found up to sqrt(x), then x is prime
-  max = Math.sqrt x
+  # Try to divide all numbers by all prime numbers up to 'x'
+  all_primes.each do |p|
+    n = 3
+    while (n <= x) do
+     if (n%p == 0) and (n != p)
+        while (n <= x) do
+          bools[n] = false
+          n += p
+        end
+        break
+      end
 
-  # Find out whether x is indeed a prime
-  (2..max).each do |i|
-    return false if x%i == 0
-  end
-
-  true
-end
-
-
-while tests = gets
-
-  tests.to_i.times do |i|
-    
-    line = gets
-    n1,n2 = line.split(' ').map { |n| n.to_i }
-
-    # Even numbers are not prime, except for 2
-    n1 += 1 if ( n1%2 == 0 ) and ( n1 != 2 )
-    n2 -= 1 if ( n2%2 == 0 ) and ( n2 != 2 )
-
-    # Handle basic cases: 1 and 2
-    n = n1
-    n += 1 if n == 1
-    if n == 2
-      puts n
-      n += 1
-    end
-
-    while ( n <= n2 )
-      puts n if is_prime n
       n += 2
     end
-    puts
-
   end
 
+  # List numbers not marked as non-prime
+  p = 3
+  primes_found = []
+  primes_found << 2 if x >= 2
+  while (p <= x) do
+    primes_found << (p) if bools[p]
+    p += 2
+  end
+
+  primes_found
 end
+
+# Uses eratosthenes sieve method to find all prime
+# numbers between interval x and y
+def primes_between( x, y )
+  return if y < 2
+  range_size = y - x + 1
+
+  bools = Array.new range_size, true
+  sqrt_primes = eratosthenes Math.sqrt(y).to_i
+
+  # Choose where to start looking for primes, ignoring even numbers
+  min_number = 0
+  min_number += 2 if (x == 1)
+  min_number += 1 if (x%2 == 0)
+
+  sqrt_primes.each do |p|
+    n = min_number
+    while (n < range_size) do
+        probable_prime = x + n
+        if (probable_prime%p == 0)
+          n += p if (probable_prime == p)
+          while (n < range_size) do
+            bools[n] = false
+            n += p
+          end
+          break
+        end
+      n += 2
+    end
+  end
+
+  # List numbers not marked as non-prime, skipping even numbers
+  n = min_number
+  puts 2 if (x <= 2) and (y >= 2)
+  while (n < range_size) do
+    puts (x + n) if bools[n]
+    n += 2
+  end
+end
+
+# Find all prime numbers in test cases
+if __FILE__ == $0
+  while tests = gets
+
+    tests.to_i.times do |i|
+      line = gets
+      n1,n2 = line.split(' ').map { |n| n.to_i }
+      primes_between(n1, n2)
+      puts
+    end
+
+    break
+
+  end
+end
+
+
