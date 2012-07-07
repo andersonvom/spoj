0bbb9e7c3ee7b1037603fb4fd082476e08d2e7f1
Anderson Mesquita <andersonvom@gmail.com>
2012-07-07 18:10:20 -0400

    IKEYB: Added comments to explain initialization

--- 0014-ikeyb/0014-ikeyb.rb
+++ 0014-ikeyb/0014-ikeyb.rb
@@ -7,16 +7,25 @@ module SPOJ
     def self.solve(keys, letters, frequencies)
       num_keys = keys.size
       num_letters = letters.size
-      best_price = Array.new(num_keys) { Array.new }
-      solution = Array.new(num_keys) { Array.new }
       cost = Array.new(num_letters) { Array.new }
+      best_price = Array.new(num_keys) { Array.new }
+      solution = Array.new(num_keys) { Array.new } # Best letter with which to start key _k_ if using _l_ letters
 
-      # initialize costs
+      # Initialize costs:
+      # This is a triangular matrix with the accumulated costs of each letter and its successors
+      # e.g.: a, ab, abc, abcd, abcde, ...
+      #           b,  bc,  bcd,  bcde, ...
+      #                c,   cd,   cde, ...
+      #                      d,    de, ...
+      #                             e, ...
       num_letters.times do |i|
         (i..num_letters-1).each do |j|
           prev_idx = (j > 0) ? j-1 : j
           cost[i][j] = cost[i][prev_idx].to_i + frequencies[j] * (j-i+1)
-          best_price[i][j] = cost[i][j] if i == 0
+          if i == 0 # trivial solution with a single key: all letters go there
+            best_price[i][j] = cost[i][j]
+            solution[i][j] = 0
+          end
         end
       end
 
