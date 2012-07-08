4bba91728a256c3abb902b49a899730d6cafc4f6
Anderson Mesquita <andersonvom@gmail.com>
2012-07-08 11:04:02 -0400

    IKEYB: improved readability of input generation file

--- 0014-ikeyb/generate_large_input.rb
+++ 0014-ikeyb/generate_large_input.rb
@@ -1,19 +1,18 @@
 #!/usr/bin/ruby
 
 FILENAME = "input_large.txt"
-num_cases = 2000
-freqs = []
-(33..126).each { |i| freqs << i.chr }
+NUM_CASES = 2000
+LETTERS = Array.new(126-33+1) { |i| (i+33).chr }
 
 info = []
-info << "#{num_cases}"
-num_cases.times do
+info << "#{NUM_CASES}"
+NUM_CASES.times do
   num_keys, num_letters = [ (rand * 90 + 1).to_i, (rand * 90 + 1).to_i ].sort
-  freqs.shuffle[0,num_letters]
+  LETTERS.shuffle[0,num_letters]
 
   info << "#{num_keys} #{num_letters}"
-  info << freqs.shuffle[0,num_keys].join
-  info << freqs.shuffle[0,num_letters].join
+  info << LETTERS.shuffle[0,num_keys].join
+  info << LETTERS.shuffle[0,num_letters].join
   num_letters.times { info << (rand * 100000).to_i }
 end
 
