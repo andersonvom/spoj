1babd4a06cf81cd384b9225a056712fb57f9741f
Anderson Mesquita <andersonvom@gmail.com>
2012-07-09 11:00:22 -0300

    IKEYB: better separation of input parameters, minor bug fixes

--- 0014-ikeyb/generate_large_input.rb
+++ 0014-ikeyb/generate_large_input.rb
@@ -2,21 +2,26 @@
 
 FILENAME = "input_large.txt"
 NUM_CASES = 2000
-ALL_CHARS = Array.new(126-33+1) { |i| (i+33).chr }
+FIRST_VALID_CHAR = 33
+LAST_VALID_CHAR = 126
+MAX_KEYS = 90
+MAX_LETTERS = 90
+MAX_FREQUENCY = 100000
 
 # sort valid chars for better visualization
+ALL_CHARS = Array.new(LAST_VALID_CHAR-FIRST_VALID_CHAR+1) { |i| (i+FIRST_VALID_CHAR).chr }
 letters = ALL_CHARS.collect{ |i| i if i =~ /[A-Za-z0-9]/ }.compact
 VALID_CHARS = letters + (ALL_CHARS - letters)
 
 info = []
 info << "#{NUM_CASES}"
 NUM_CASES.times do
-  num_keys, num_letters = [ (rand * 90 + 1).to_i, (rand * 90 + 1).to_i ].sort
+  num_keys, num_letters = [ rand(MAX_KEYS)+1, rand(MAX_LETTERS)+1 ].sort
 
   info << "#{num_keys} #{num_letters}"
   info << VALID_CHARS[0,num_keys].join
   info << VALID_CHARS[0,num_letters].join
-  num_letters.times { info << (rand * 100000).to_i }
+  num_letters.times { info << rand(MAX_FREQUENCY+1) }
 end
 
 begin
