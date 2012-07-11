52f22d974f4da5385fcafdaf216e4eeb75242457
Anderson Mesquita <andersonvom@gmail.com>
2012-07-10 21:37:04 -0400

    FCTRL: calculating _z(n)_ in a naive way for educational/testing purposes.

--- 0011-fctrl/0011-fctrl.rb
+++ 0011-fctrl/0011-fctrl.rb
@@ -15,6 +15,22 @@ module SPOJ
       factorial
     end
 
+    # For educational/testing purposes only, calculate the
+    # number of zeroes of the actual factorial of _n_
+    # e.g.: z_naive(50) # => 12
+    def self.z_naive(n)
+      temp = 1
+      num_zeroes = 0
+      (2..n).each do |i|
+        temp *= i
+        while temp%10 == 0
+          temp /= 10  # since factorials grow too big, keep it small so that it won't overflow
+          num_zeroes += 1
+        end
+      end
+      num_zeroes
+    end
+
     def self.z(n)
     end
 
